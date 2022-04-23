2a2d0e7fae5c8e81b6abb96a0bb1eacf58244d6d
Olliemint <furywrld5@gmail.com>
2022-04-23 13:18:43 +0300

    added input function for user to enter username and password

--- run.py
+++ run.py
@@ -7,3 +7,21 @@ def main():
     while True:
         print("Welcome to Password-Locker")
         print('\n')
+        print("Please select a short code to cruise through:To create new user use 'NU': For logging in to your account use 'LG':And finally to exit use 'EX'")
+        shortcode = input().upper()
+        print('\n')
+        
+        
+        if shortcode == 'NU':
+            
+            print("Create your Username:")
+            entered_username = input()
+            
+            print("Create your Password")
+            entered_password = input()
+            
+            print("Confirm your Password")
+            confirm_password = input()
+            
+            
+        
