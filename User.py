05921526d055bdfbba9e0142a052f66ad63a667a
Olliemint <furywrld5@gmail.com>
2022-04-23 12:42:27 +0300

    feat: adding method that takes properties and saving new user

--- User.py
+++ User.py
@@ -1,8 +1,28 @@
 class User:
     """
-    class to generate objects
+    class to generate objects/new instance of User
     """
     user_list = []
     """
     Empty user list for saving user objects
     """
+    
+    def __init__(self,username,password):
+        
+        """
+        method that defines properties of the objects and we can pass the Args
+        """
+        
+        self.username = username
+        self.password =  password
+    
+    
+    def add_new_user(self):
+        
+        """
+        saves user objects to user_list
+        """   
+        
+        User.user_list.append(self) 
+        
+        
