d139b2cd0b1bb5128e52327b6d361aa7d5011fdb
Olliemint <furywrld5@gmail.com>
2022-04-24 13:31:04 +0300

    Test if the user is saved

--- User_Credentials_test.py
+++ User_Credentials_test.py
@@ -20,11 +20,48 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("user001","2580")   # create new object
+
+
+    
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list= []
+    
+    
+    
     
     
     def test_init(self):
+        
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        
         self.assertEqual(self.new_user.username,"user001")
         self.assertEqual(self.new_user.password,"2580") 
+        
+        
+    def test_add_new_user(self):
+        
+        '''
+        test_add_new_user test case to test if the contact object is saved into
+         the user list
+        '''
+        
+        self.new_user.add_new_user()  #saving new user
+        
+        self.assertEqual(len(User.user_list),1)
+        
+        
+             
+        
+        
+        
+        
+        
+        
         
         
 if __name__ == '__main__':
